99b0b4bdf5a9165bdd57f0c750e1a03eeb9f4f84
Grzegorz Ewald <grzegorz.ewald@gforces.pl>
2017-10-10 13:04:12 +0200

    Direct raw Kinesis stream data streaming added

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 A repository with Snowplow recovery mechanism for Realtime data pipeline S3 backup file.
 
 ## How to use
-The Python code itself process gzipped backup files and prints to stdout bas64 encoded records. Thic cna be piped into stream enrich, working on pipe stream and writing to Enriched steram.
+Recovery emchanism can either emit base64 encoded records to stdout or write directly to the kinesis stream.
 
 Backup raw files may be picked up either form S3 bucket (with file name prefix or not) or form a local file (currently only a single one supported).
 
 ## How to fix bad records?
 
-Go to line 22 of recoverty.py and modify records as much as you need/want. Note, you need to uncomment lines 21 and 23 as well.
+Line 48 is the answer.
 
 ## Notes
 _Thrift_ schema is taken from official Snowplow repo: [collector-payload.thrift](https://github.com/snowplow/snowplow/blob/master/2-collectors/thrift-schemas/collector-payload-1/src/main/thrift/collector-payload.thrift)
--- recovery.py
+++ recovery.py
@@ -13,17 +13,41 @@ from thriftpy.utils import deserialize, serialize
 RECORD_START_SEQUENCE = b'\x0b\x00\x64\x00\x00\x00'
 TMP_FILE_NAME = 'tmp.gz'
 
+client = boto3.client('kinesis')
+
 collector = thriftpy.load("collector-payload.thrift")
 collector_payload = collector.CollectorPayload()
+
+def emit_to_stdout(record, key):
+    print base64.b64encode(str(record))
+
+def emit_to_kinesis(record, key):
+    resp = client.put_record(
+        StreamName = emiter_stream_name,
+        Data = record,
+        PartitionKey = key
+    )
+
+emiters = {
+    "stdout": emit_to_stdout,
+    "kinesis": emit_to_kinesis
+}
+
+emiter = 'stdout'
+emiter_stream_name = ''
+
+def emit(record, key):
+    emiters[emiter](record, key)
 
 def eprint(args):
     print >> sys.stderr, args
 
 def process_record(record):
-    # decoded_record = deserialize(collector_payload, record, TCyBinaryProtocolFactory())
-    # decoded_record.userAgent = u'updated UserAgent string'
-    # record = serialize(decoded_record, TCyBinaryProtocolFactory())
-    print base64.b64encode(str(record))
+    decoded_record = deserialize(collector_payload, record, TCyBinaryProtocolFactory())
+    ipAddress = str(decoded_record.ipAddress)
+    decoded_record.collector = 'S3-recovery-0.0.2'
+    record_to_emit = serialize(decoded_record, TCyBinaryProtocolFactory())
+    emit(record_to_emit, ipAddress)
 
 def process_gzfile(gz_file_name):
     with gzip.open(gz_file_name,'rb') as f:
@@ -50,7 +74,16 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--bucket", type=str,  help="S3 bucket", required=False)
     parser.add_argument("-p", "--prefix", type=str,  help="S3 file prefix", required=False)
     parser.add_argument("-f", "--local_file", type=str,  help="Local file name", required=False)
+    parser.add_argument("-o", "--output", type=str,  help="Output stream type", required=False)
+    parser.add_argument("-s", "--output_stream", type=str,  help="Output stream name", required=False)
     args = parser.parse_args()
+    if args.output in emiters.keys():
+        emiter = args.output
+    else:
+        eprint("Invalid output type!")
+        sys.exit(1)
+    if isinstance(args.output_stream, str) and (len(args.output_stream) > 0):
+        emiter_stream_name = args.output_stream
     if args.local_file and (args.bucket or args.prefix):
         eprint("Cant use local file and s3 files at the same time!")
         sys.exit(2)
@@ -60,5 +93,5 @@ if __name__ == "__main__":
     if args.local_file:
         process_gzfile(args.local_file)
         sys.exit(0)
-    eprint("No parameter set!")
+    eprint("No required parameter set!")
     sys.exit(1)
