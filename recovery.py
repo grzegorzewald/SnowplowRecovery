0d7ece0cf916219d2a491e9a3dd3fe6a20e112ab
Grzegorz Ewald <grzegorz.ewald@gforces.pl>
2017-10-10 15:41:23 +0200

    Python 3 compatybility. PEP8 compatybility

--- recovery.py
+++ recovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from __future__ import print_function
 import sys
 import argparse
 import gzip
@@ -18,39 +18,45 @@ client = boto3.client('kinesis')
 collector = thriftpy.load("collector-payload.thrift")
 collector_payload = collector.CollectorPayload()
 
-def emit_to_stdout(record, key):
-    print base64.b64encode(str(record))
+
+def emit_to_stdout(record, _):
+    print(base64.b64encode(str(record)))
+
 
 def emit_to_kinesis(record, key):
-    resp = client.put_record(
-        StreamName = emiter_stream_name,
-        Data = record,
-        PartitionKey = key
+    client.put_record(
+        StreamName=emitter_stream_name,
+        Data=record,
+        PartitionKey=key
     )
 
-emiters = {
+emitters = {
     "stdout": emit_to_stdout,
     "kinesis": emit_to_kinesis
 }
 
-emiter = 'stdout'
-emiter_stream_name = ''
+emitter = 'stdout'
+emitter_stream_name = ''
+
 
 def emit(record, key):
-    emiters[emiter](record, key)
+    emitters[emitter](record, key)
 
-def eprint(args):
-    print >> sys.stderr, args
+
+def eprint(message):
+    print(message, file=sys.stderr)
+
 
 def process_record(record):
     decoded_record = deserialize(collector_payload, record, TCyBinaryProtocolFactory())
-    ipAddress = str(decoded_record.ipAddress)
+    ip_address = str(decoded_record.ipAddress)
     decoded_record.collector = 'S3-recovery-0.0.2'
     record_to_emit = serialize(decoded_record, TCyBinaryProtocolFactory())
-    emit(record_to_emit, ipAddress)
+    emit(record_to_emit, ip_address)
+
 
 def process_gzfile(gz_file_name):
-    with gzip.open(gz_file_name,'rb') as f:
+    with gzip.open(gz_file_name, 'rb') as f:
         record = bytearray(f.read(6))
         byte = f.read(1)
         while byte != "":
@@ -61,13 +67,15 @@ def process_gzfile(gz_file_name):
             byte = f.read(1)
         process_record(record)
 
+
 def process_s3(s3_bucket, s3_prefix):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(s3_bucket)
-    for obj in bucket.objects.filter(Prefix = s3_prefix):
+    for obj in bucket.objects.filter(Prefix=s3_prefix):
         eprint(obj.key)
         bucket.download_file(obj.key, TMP_FILE_NAME)
         process_gzfile(TMP_FILE_NAME)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -77,18 +85,18 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", type=str,  help="Output stream type", required=False)
     parser.add_argument("-s", "--output_stream", type=str,  help="Output stream name", required=False)
     args = parser.parse_args()
-    if args.output in emiters.keys():
-        emiter = args.output
+    if args.output in emitters.keys():
+        emitter = args.output
     else:
         eprint("Invalid output type!")
         sys.exit(1)
     if isinstance(args.output_stream, str) and (len(args.output_stream) > 0):
-        emiter_stream_name = args.output_stream
+        emitter_stream_name = args.output_stream
     if args.local_file and (args.bucket or args.prefix):
         eprint("Cant use local file and s3 files at the same time!")
         sys.exit(2)
     if args.bucket:
-        process_s3(s3_bucket = args.bucket, s3_prefix = args.prefix or '')
+        process_s3(s3_bucket=args.bucket, s3_prefix=args.prefix or '')
         sys.exit(0)
     if args.local_file:
         process_gzfile(args.local_file)
